e343e7ebf3e4990c54e402c996f2b7d96c9408a1
Zsolt <Zsolt veges>
2017-11-22 16:35:44 +0100

    Change the range(len()) to enumerate

--- bowling.py
+++ bowling.py
@@ -2,28 +2,28 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+    for index, actual_point in enumerate(game):
+        if actual_point == '/':
+            result += 10 - last_point
         else:
-            result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(actual_point)
+        if frame < 10  and get_value(actual_point) == 10:
+            if actual_point == '/':
+                result += get_value(game[index+1])
+            elif actual_point == 'X' or actual_point == 'x':
+                result += get_value(game[index+1])
+                if game[index+2] == '/':
+                    result += 10 - get_value(game[index+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[index+2])
+        last_point = get_value(actual_point)
         if not in_first_half:
             frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if actual_point == 'X' or actual_point == 'x':
             in_first_half = True
             frame += 1
     return result
